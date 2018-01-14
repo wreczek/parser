3611324f7ddf4093e4976462399a13b51a7cb1fa
Wiktor <neuromind@vp.pl>
2018-01-14 19:13:52 +0100

    Update Article.java

--- src/Main/p1/Article.java
+++ src/Main/p1/Article.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Article
 {
     private List<String> text = new ArrayList<>();
-    private List<SmallObject> cubes = new ArrayList<>();  //zrobic private
+    private List<SmallObject> cubes = new ArrayList<>();
     private String name;
     private Type type;
     private String dzial;
@@ -16,16 +16,16 @@ public class Article
 
     Article(){}
 
-    Article (String string,String dzial,String rozdzial)
+    Article (String string, String dzial, String rozdzial)
     {
         this.dzial = dzial;
         this.rozdzial = rozdzial;
         type = Type.Artykul;
         name = cutName(string);
-        text.add(getContent(string));
+        text.add(string);
     }
 
-    public void removeLastConetent(){
+    public void removeLastContent(){
         text.remove(text.size()-1);
     }
 
@@ -37,15 +37,18 @@ public class Article
         return rozdzial;
     }
 
-    public String getContent(String string){
-        // wycina tekst ale tylko podczas tworzenia obiektu
-        return string;
-    }
+//    public String getContent(String string)
+//    {
+//        // wycina tekst ale tylko podczas tworzenia obiektu
+//        return string;
+//    }
 
-    public String cutName(String string){
+    public String cutName(String string)
+    {
 
         String result = ".";
-        for(int i = 5;i<string.length();i++){
+        for(int i = 5; i < string.length(); i++)
+        {
             if(string.charAt(i) == '.') break;
             result += string.charAt(i);
         }
@@ -60,16 +63,18 @@ public class Article
         cubes.add(object);
     }
 
-    public SmallObject searchSmallObject(Type type){
+    public SmallObject searchSmallObject(Type type)
+    {
         if(cubes.size() == 0 || this.type == type) return null;
         return cubes.get(cubes.size()-1).searchSmallObject(type);
     }
 
-    public void write(){
+    public void write()
+    {
         Check.set();
-          for(int i=0;i<text.size();i++)
+        for(int i = 0 ; i < text.size(); i++)
             System.out.println(text.get(i));
-        for(int i = 0;i<cubes.size();i++)
+        for(int i = 0; i < cubes.size(); i++)
             cubes.get(i).write();
     }
 
@@ -77,8 +82,9 @@ public class Article
         return name;
     }
 
-    public void search(String punkt,String podpunkt,String litera){
-        for(int i = 0;i<cubes.size();i++)
-            cubes.get(i).search(punkt,podpunkt,litera);
+    public void search(String punkt, String podpunkt, String litera)
+    {
+        for(int i = 0; i < cubes.size(); i++)
+            cubes.get(i).search(punkt, podpunkt, litera);
     }
 }
